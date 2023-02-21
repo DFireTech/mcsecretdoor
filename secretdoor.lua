841eb03bb7f07d29988a00577f0f82e47b446dae
DFireTech <79111709+DFireTech@users.noreply.github.com>
2023-02-20 22:20:20 -0600

    Create secretdoor.lua

---
+++ secretdoor.lua
@@ -0,0 +1,12 @@
+local password = "cleanRoom"
+
+io.write("Please enter the password: ")
+local pInput = io.read()
+repeat
+    if pInput ~= password then
+            io.write("Incorrect. Please try again")
+    end
+until (pInput==password)
+redstone.setOutput("back",true)
+sleep(10)
+redstone.setOutput("back",false)
