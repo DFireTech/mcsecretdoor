d4b1170ffcaf9aa4cadf758afa6e61554994f08e
DFireTech <79111709+DFireTech@users.noreply.github.com>
2023-02-22 19:04:28 -0600

    Update secretdoor.lua

--- secretdoor.lua
+++ secretdoor.lua
@@ -1,12 +1,29 @@
 local password = "cleanRoom"
+local entries = 0
+local maxEntries = 3
 
-io.write("Please enter the password: ")
-local pInput = io.read()
-repeat
-    if pInput ~= password then
-            io.write("Incorrect. Please try again")
+io.write("Please enter the password.. ")
+local pEntry = io.read()
+
+while pEntry ~= password
+do
+    entries = entries + 1
+        if entries >= maxEntries then
+            print("Max attempts reached, exiting")
+            sleep(2)
+            print("Shutting down...")
+            sleep(2)
+            os.shutdown()
+        end
+    print("Incorrect, try again..")
+    sleep(1)
+    print(string.format("%d attempts remaining - ",(maxEntries - entries)))
+    sleep(1)
+    io.write("Please enter the password.. ")
+    pEntry = io.read()
 end
-until (pInput==password)
-redstone.setOutput("back",true)
-sleep(10)
-redstone.setOutput("back",false)
+print("Access granted.")
+rs.setOutput("back",true)
+sleep(5)
+rs.setOutput("back",false)
+exit()
